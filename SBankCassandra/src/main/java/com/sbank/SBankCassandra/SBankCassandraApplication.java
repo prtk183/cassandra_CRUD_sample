00ebfc891dfb3ffbc60b070f90b768098c92f117
prtk183 <prateekgawarle183@gmail.com>
2018-07-09 08:09:55 +0530

    CRUD

---
+++ SBankCassandra/src/main/java/com/sbank/SBankCassandra/SBankCassandraApplication.java
@@ -0,0 +1,12 @@
+package com.sbank.SBankCassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBankCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBankCassandraApplication.class, args);
+	}
+}
